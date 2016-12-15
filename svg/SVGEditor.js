9dc1cb50baef101204fe1bd923ff95b420abe52d
cy0707 <674986077@qq.com>
2016-12-15 17:31:14 +0800

    svg的编辑器的页面结构的搭建

---
+++ svg/SVGEditor.js
@@ -0,0 +1,10 @@
+;(function(){
+	//获取元素
+	var showShape = document.querySelector('.action-show'), //展示元素
+		rectButton = document.getElementById('rect'), //创建正方形按钮
+		circleButton = document.getElementById('circle'), //圆
+		ellipseButton = document.getElementById('ellipse'), //椭圆
+		lineButton = document.getElementById('line'),//线
+		createShape = document.querySelector('.create-shape'), //创建形状
+
+})()
